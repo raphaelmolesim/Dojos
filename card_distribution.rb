4cd5a0f78440baa1f18238422a8a4960cb3dfd1c
Raphael Molesim <raphael.sm86@gmail.com>
2011-05-01 12:59:53 +0100

    Translated to English

--- card_distribution.rb
+++ card_distribution.rb
@@ -2,15 +2,15 @@ class Array
   def sum() inject(0) { |sum, i| sum + i } end
 end
 
-def cria_cartao passagens
-  cartao = []
-  passagens.each { |passagem| cartao << passagem if cartao.sum + passagem <= 150 }
-  cartao.each { |passagem| passagens.delete passagem }
+def create_card tickets
+  card = []
+  tickets.each { |ticket| card << ticket if card.sum + ticket <= 150 }
+  card.each { |ticket| tickets.delete ticket }
 end
 
-passagens = [ 30.98, 95.85, 61.99, 32.99, 64.79, 54.00, 55.00 ]
-resultado = [ ]
-3.times { resultado << cria_cartao(passagens) }
-resultado.each { |cartao| puts "Cartao: #{cartao.join(" ")} => #{cartao.sum}" }
-puts "==> Valor total: #{resultado.flatten.sum}"
-puts("==> #{passagens.empty? ? 'Todas passagem couberam' : 'Restou #{passagens.size} passagens'}!")
+tickets = [ 30.98, 95.85, 61.99, 32.99, 64.79, 54.00, 55.00 ]
+cards = [ ]
+3.times { cards << create_card(tickets) }
+cards.each { |card| puts "Card: #{card.join(" ")} => #{card.sum}" }
+puts "==> Total amount: #{cards.flatten.sum}"
+puts("==> #{tickets.empty? ? 'All tickets fitted' : '#{tickets.size} tickets remaining'}!")
